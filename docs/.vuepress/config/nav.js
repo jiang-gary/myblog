517a45105480bcbb121ab91905f53b7f7c2fcbce
jiangrui <1016875496@qq.com>
2022-08-30 23:42:40 +0800

    使用第三方主题

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    text: "Home",
+    link: "/",
+  },
+  {
+    text: "学习",
+    items: [
+      { text: "Javascript", link: "/pages/5ea030/" },
+      { text: "Vue" },
+      { text: "React" },
+    ],
+  },
+  {
+    text: "工作",
+  },
+  {
+    text: "生活",
+  },
+];
