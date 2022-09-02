7ac2b9f470eba800728c60c7a3ef2d1b6299c097
jiangrui <1016875496@qq.com>
2022-09-02 12:04:53 +0800

    add new page

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -9,6 +9,9 @@ module.exports = [
       { text: "Javascript", link: "/pages/5ea030/" },
       { text: "Vue" },
       { text: "React" },
+      { text: "小程序", link: "/pages/51014f/" },
+      { text: "前端工程", link: "/pages/209fb8/" },
+      { text: "前端组件库", link: "/pages/97fe03/" },
     ],
   },
   {
